68896ed37f0a6476526e81210fa1cd18e8a4f288
Kornelia <korneliav2@gmail.com>
2026-06-23 00:49:45 +0200

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import WelcomePage from "./pages/login/WelcomePage";
 import RegisterPage from "./pages/login/RegisterPage";
 import LoginPage from "./pages/login/LoginPage";
 import ConfirmEmailPage from "./pages/login/ConfirmEmailPage";
 import HomePage from "./pages/user/HomePage";
-import AdminHomePage from "./pages/admin/AdminHomePage";
 import ProfilePage from "./pages/user/ProfilePage";
 import MovieDetailsPage from "./pages/user/MovieDetailsPage";
 import BookingPage from "./pages/user/BookingPage";
 import TicketsPage from "./pages/user/TicketsPage";
+import AdminHomePage from "./pages/admin/AdminHomePage";
 import AdminHallsPage from "./pages/admin/AdminHallsPage";
 import AdminMoviesPage from "./pages/admin/AdminMoviesPage";
 import AdminScreeningsPage from "./pages/admin/AdminScreeningsPage";
@@ -21,31 +21,24 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<WelcomePage />} />
+                <Route path="/" element={<WelcomePage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/confirm-email" element={<ConfirmEmailPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
 
-                {/* Nowy widok rejestracji z dynamicznymi regułami haseł */}
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/confirm-email" element={<ConfirmEmailPage />} />
+                <Route path="/home" element={<HomePage/>}/>
+                <Route path="/movie/:id" element={<MovieDetailsPage/>}/>
+                <Route path="/booking/:movieId" element={<BookingPage/>}/>
+                <Route path="/tickets" element={<TicketsPage/>}/>
+                <Route path="/profile" element={<ProfilePage/>}/>
 
-                {/* Nowy widok logowania w React Web */}
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/adminHome" element={<AdminHomePage/>}/>
+                <Route path="/adminHalls" element={<AdminHallsPage/>}/>
+                <Route path="/adminMovies" element={<AdminMoviesPage/>}/>
+                <Route path="/admin/screenings/:movieId" element={<AdminScreeningsPage/>}/>
+                <Route path="/adminTickets" element={<AdminTicketsPage/>}/>
 
-                <Route path="/home" element={<HomePage />} />
-                {/*<Route path="/movie" element={<MovieDetailsPage />} />*/}
-                <Route path="/movie/:id" element={<MovieDetailsPage />} />
-                <Route path="/booking/:movieId" element={<BookingPage />} />
-                <Route path="/tickets" element={<TicketsPage />} />
-
-                <Route path="/profile" element={<ProfilePage />} />
-
-                <Route path="/adminHome" element={<AdminHomePage />} />
-                <Route path="/adminHalls" element={<AdminHallsPage />} />
-                <Route path="/adminMovies" element={<AdminMoviesPage />} />
-                <Route path="/admin/screenings/:movieId" element={<AdminScreeningsPage />} />
-                <Route path="/adminTickets" element={<AdminTicketsPage />} />
-
-                <Route path="/access-denied" element={<AccessDeniedPage />} />
-                {/* Poprzednia ścieżka do sprawdzania połączenia ze Spring Bootem */}
+                <Route path="/access-denied" element={<AccessDeniedPage/>}/>
             </Routes>
         </BrowserRouter>
     );
